6afce2360f784e1d5bc3896a38b19c2dbaf3f93e
ViniBelo <vinialvesbelo@outlook.com>
2026-06-09 15:48:39 -0300

    feat(product): add GET /products swagger documentation

--- src/main/java/br/com/devpasso/order_management/api/controller/ProductsController.java
+++ src/main/java/br/com/devpasso/order_management/api/controller/ProductsController.java
@@ -4,18 +4,18 @@ import br.com.devpasso.order_management.application.dto.PaginatedResponse;
 import br.com.devpasso.order_management.application.dto.ProductResponse;
 import br.com.devpasso.order_management.application.mapper.ProductResponseMapper;
 import br.com.devpasso.order_management.application.usecase.ListProductsUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping( "/v1/products")
+@Tag( name = "Products", description = "Operations about products" )
 public class ProductsController {
     private final ListProductsUseCase listProductsUseCase;
     private final ProductResponseMapper mapper;
@@ -27,8 +27,11 @@ public class ProductsController {
     }
 
     @GetMapping
+    @Operation( summary = "List all products" )
     public ResponseEntity<PaginatedResponse<ProductResponse>> listAll(
-            @PageableDefault(size = 20, sort = "name") Pageable pageable
+            @ParameterObject
+            @PageableDefault(size = 20, sort = "name")
+            Pageable pageable
     ) {
         Page<ProductResponse> products = listProductsUseCase.execute(pageable)
                 .map(mapper::toResponse);
