eb2577ffb6e5b53c762b633e6ff46b8e0d4833f9
ViniBelo <vinialvesbelo@outlook.com>
2026-06-11 21:16:34 -0300

    refactor(product): @Param annotation to name filter

--- src/main/java/br/com/devpasso/order_management/infrastructure/persistence/repository/ProductJpaRepository.java
+++ src/main/java/br/com/devpasso/order_management/infrastructure/persistence/repository/ProductJpaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -12,5 +13,5 @@ import java.util.UUID;
 @Repository
 public interface ProductJpaRepository extends JpaRepository<Product, UUID> {
     @Query("SELECT p FROM Product p WHERE p.name LIKE %:name%")
-    Page<Product> findAll(Pageable pageable, String name);
+    Page<Product> findAll(Pageable pageable, @Param("name") String name);
 }
