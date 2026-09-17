8a379907278fb859499340b4fabf74595a5324b5
ViniBelo <vinialvesbelo@outlook.com>
2026-09-17 19:26:58 -0300

    refactor(product): add validations to PaginationQuery attributes

--- src/main/java/br/com/devpasso/order_management/domain/common/PaginationQuery.java
+++ src/main/java/br/com/devpasso/order_management/domain/common/PaginationQuery.java
@@ -1,5 +1,13 @@
 package br.com.devpasso.order_management.domain.common;
 
-public record PaginationQuery (int page,
-                               int size,
-                               String sort) { }
+public record PaginationQuery (
+        int page,
+        int size,
+        String sort
+) {
+    public PaginationQuery {
+        if (page < 0) throw new IllegalArgumentException("Page must be >= 0");
+        if (size <= 0) throw new IllegalArgumentException("Size must be > 0");
+        if (size > 1000) throw new IllegalArgumentException("Size must be <= 1000");
+    }
+}
